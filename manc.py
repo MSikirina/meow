0f9c976bd708a66f09d48bea00aacd8142f1a6a5
gp21 <galkamoroz@rambler.ru>
2026-02-06 09:30:50 +0300

    первый пробный

--- manc.py
+++ manc.py
@@ -5,4 +5,15 @@ def c_cmat(xmin, xmax, ymin, ymax, den):
     im=np.linspace(ymin, ymax, int((ymax-ymin)*den))
     mat=re[np.newaxis,:]+im[:,np.newaxis]*1j
     return mat
-print(c_cmat(-2,2.1,-2,2,10))
+#print(c_cmat(-2,2.1,-2,2,10))
+
+def stable(mat, n):
+    z=0
+    for i in range(n):
+        z=z**2+mat
+    return abs(z)<=2
+c=c_cmat(-2, 0.5, -1.5, 1.5, 2000)
+man=stable(c, 35)
+plt.imshow(man, cmap='jet')
+plt.gca().set_aspect('equal')
+plt.show()
